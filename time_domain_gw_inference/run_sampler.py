db2b2fdaa7d1fa82fdc82b8d89b6404b045553e6
Sophie Hourihane <sophie.hourihane@ligo.org>
2024-08-09 16:00:07 -0700

    removed unneeded ACF calculation from get_conditioned_time_and_data

--- time_domain_gw_inference/run_sampler.py
+++ time_domain_gw_inference/run_sampler.py
@@ -404,12 +404,6 @@ def get_conditioned_time_and_data(args, wf_manager, reference_parameters, initia
         time_dict[ifo] = time_dict[ifo][idx - Npre:idx + Npost]
         data_dict[ifo] = data_dict[ifo][idx - Npre:idx + Npost]
 
-    # Calculate ACF
-    rho_dict, cond_psds = utils.get_ACF(pe_psds, time_dict, f_low=args.flow, f_max=args.fmax, return_psds=True)
-
-    for ifo, rho in rho_dict.items():
-        assert len(rho) == len(data_dict[ifo]), 'Length for ACF is not the same as for the data'
-
     return time_dict, data_dict, pe_psds
 
 
