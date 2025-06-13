11e70f37e4e6dd43d05859c4afae80d619db4618
Simona Miller <simona.miller@ldas-pcdev5.ligo.caltech.edu>
2025-06-13 16:06:19 -0700

    add back in waveform generation script

---
+++ time_domain_gw_inference/waveform_h5s.py
@@ -0,0 +1,131 @@
+import sys
+import os
+from time_domain_gw_inference import run_sampler, utils, group_postprocess
+import re
+import numpy as np
+import scipy
+import pandas as pd
+import multiprocessing as mp
+import h5py
+import argparse
+import tqdm
+
+
+def save_waveform_h5py(output_file, waveform_dict_list, maxL_waveform_dict):
+    """
+    save list of waveform dicts
+    """
+    # Save output to HDF5
+    with h5py.File(output_file, 'w') as f:
+        
+        # maxL 
+        group = f.create_group('maxL')
+        for key, value in maxL_waveform_dict.items():
+            group.create_dataset(key, data=value) 
+        
+        # random draws
+        for i, wf_dict in enumerate(waveform_dict_list):
+            group = f.create_group(f'waveform_{i}')
+            for key, value in wf_dict.items():
+                group.create_dataset(key, data=value)
+        
+def load_waveform_h5py(output_file):
+    # Load HDF5 file if it exists and return output
+    with h5py.File(output_file, 'r') as f:
+        waveform_dict_list = []
+        for key in f.keys():
+            group = f[key]
+            wf_dict = {k: group[k][()] for k in group.keys()}
+            waveform_dict_list.append(wf_dict)
+    return waveform_dict_list
+
+def compute_waveform(i, data_frame, likelihood_manager):
+    params = data_frame.iloc[i]
+    return group_postprocess.get_waveform_dict(params, likelihood_manager, ifos=likelihood_manager.ifos)
+
+def get_waveform_filename(directory, run_key):
+    """ pass in directory where runs are stored, not the waveform directory"""
+    waveform_dir = os.path.join(directory, 'waveforms')
+    return os.path.join(waveform_dir, f'{run_key}_waveforms.h5')
+
+def make_waveform_h5_arg_parser():
+    parser = argparse.ArgumentParser(description="Script to save waveforms to file")
+
+    # Add arguments
+    parser.add_argument("--directory", type=str, help="Input directory")
+    parser.add_argument("--run_key", type=str, help="name of run for which to create waveforms")
+
+    parser.add_argument("--overwrite", action="store_true",
+                        help="Flag to overwrite existing files (default: False)")
+    parser.add_argument("--N_waveforms", type=int, default=300, help="Number of waveforms (default: 300)")
+    parser.add_argument("--ncpu", type=int, default=mp.cpu_count(), help="Number of parallel processes to start")
+    return parser
+
+
+if __name__ == "__main__":
+    # Initialize ArgumentParser
+    parser = make_waveform_h5_arg_parser()
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Assign arguments to variables
+    directory = args.directory
+    overwrite = args.overwrite
+    N_waveforms = args.N_waveforms
+
+    # don't overwrite existing files unless asked to explicitly
+    waveform_filename = get_waveform_filename(directory, args.run_key)
+    if not overwrite and waveform_filename is not None and os.path.exists(waveform_filename):
+        # print('loading from existing file')
+        print('waveform already exists, use --overwrite in order to overwrite all the files!')
+        exit()
+
+    # load full run (for the waveform generator)
+    filename_dict = group_postprocess.generate_filename_dict(directory)
+    full_parser = run_sampler.create_run_sampler_arg_parser()
+    full_args, \
+    full_kwargs, \
+    full_likelihood_manager = group_postprocess.get_settings_from_command_line_file(
+        os.path.join(directory, 'command_line.sh'),
+        filename_dict['full'],
+        directory + '/',
+        full_parser, verbose=True)
+    
+    print(filename_dict)
+
+    sub_directory = filename_dict[args.run_key]
+    dataframe = group_postprocess.load_dataframe(directory, sub_directory)
+
+    waveform_dir = os.path.join(directory, 'waveforms')
+    if not os.path.exists(waveform_dir):
+        os.mkdir(waveform_dir)
+        
+    # Find maxL waveform
+    i_max_logL = np.argmax(dataframe['ln_posterior'] - dataframe['ln_prior'])
+    max_logL_params = dataframe.iloc[i_max_logL]
+    
+    maxL_wf_dict = full_likelihood_manager.waveform_manager.get_projected_waveform(
+        max_logL_params,
+        full_likelihood_manager.ifos, 
+        time_dict = full_likelihood_manager.time_dict ,
+        f22_start = full_likelihood_manager.f22_start, 
+        f_ref = full_likelihood_manager.f_ref
+    )
+        
+    # Generate a bunch
+    if N_waveforms == len(dataframe): 
+        print('generating reconstructions for all the waveforms')
+        rand_ints = np.arange(N_waveforms)
+    else:
+        rand_ints = np.random.randint(len(dataframe), size=N_waveforms)
+
+    # Prepare the arguments for starmap
+    parallel_args = [(i, dataframe, full_likelihood_manager) for i in rand_ints]
+
+    with mp.Pool(processes=args.ncpu) as pool:
+        # Use pool.starmap to parallelize the computation
+        results = list(pool.starmap(compute_waveform, tqdm.tqdm(parallel_args, total=N_waveforms)))
+
+    save_waveform_h5py(waveform_filename, results, maxL_wf_dict)
+    print("all done! waveforms saved to:", waveform_filename)
