8c95a8a42d441897d7edf462da6eabef134487ee
Simona Miller <smiller@rusty2.cm.cluster>
2025-07-22 20:13:40 -0400

    modularize postprocessing script to be used better in other repositories

--- time_domain_gw_inference/utils/postprocessing.py
+++ time_domain_gw_inference/utils/postprocessing.py
@@ -35,35 +35,12 @@ def get_dict_from_samples(samples, parameter_manager, **kwargs):
     return pandas.DataFrame(samps_phys)
 
 
-def postprocess_samples(sampler, likelihood_manager, getRidOfFixed=False, **kwargs):
+def postprocessing_get_complete_samples_dict(samples, samples_lnp, likelihood_manager, getRidOfFixed=False, **kwargs): 
 
-    """
-    Post-process emcee sample chains
-    
-    input: sampler = emcee sampler object
-    """
-    
-    print('\nPOSTPROCESSING:')
-    
-    # Get autocorrelation time
-    tau = sampler.get_autocorr_time(quiet=True)
-    try:
-        burnin = max(int(5 * np.max(tau)), 0)
-        thin = max(int(0.5 * np.min(tau)), 1)
-        print(f'burnin = {burnin}, thin = {thin}')
-    except:
-        print('WARNING thinning by 1, 0 burnin!')
-        burnin = 0
-        thin = 1
-        
-    samples = sampler.get_chain(discard=burnin, flat=True, thin=thin)
-    print('samples shape:', samples.shape)
-    
-    # Convert samples into their physical quantities
+    # convert samples into their physical quantities
     samples_dict = get_dict_from_samples(samples, likelihood_manager.log_prior, **kwargs)
 
     # Add posterior values the sample_dict
-    samples_lnp = sampler.get_log_prob(discard=burnin, flat=True, thin=thin)
     samples_dict['ln_posterior'] = samples_lnp
     
     # Add prior values to the sample_dict
@@ -84,5 +61,39 @@ def postprocess_samples(sampler, likelihood_manager, getRidOfFixed=False, **kwar
     # Get rid of the fixed parameters if we want
     if getRidOfFixed:
         samples_dict = samples_dict[[k for k, v in samples_dict.items() if v.min() != v.max()]]
+
+    return samples_dict
+
+def postprocess_samples(sampler, likelihood_manager, getRidOfFixed=False, **kwargs):
+    """
+    Post-process emcee sample chains
+    
+    input: sampler = emcee sampler object
+    """
+    
+    print('\nPOSTPROCESSING:')
+    
+    # Get autocorrelation time
+    tau = sampler.get_autocorr_time(quiet=True)
+    try:
+        burnin = max(int(5 * np.max(tau)), 0)
+        thin = max(int(0.5 * np.min(tau)), 1)
+        print(f'burnin = {burnin}, thin = {thin}')
+    except:
+        print('WARNING thinning by 1, 0 burnin!')
+        burnin = 0
+        thin = 1
+        
+    # thin and burn
+    samples = sampler.get_chain(discard=burnin, flat=True, thin=thin)
+    print('samples shape:', samples.shape)
+
+    # get associated log probs
+    samples_lnp = sampler.get_log_prob(discard=burnin, flat=True, thin=thin)
+
+    # format everything correctly
+    samples_dict = postprocessing_get_complete_samples_dict(
+        samples, samples_lnp, likelihood_manager, getRidOfFixed=getRidOfFixed, **kwargs
+    )
     
     return samples_dict
