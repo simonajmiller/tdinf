52e4e46480ebc744348f63c66ae7ed9742fafc1e
Simona Miller <102549928+simonajmiller@users.noreply.github.com>
2025-08-11 14:03:09 -0700

    update filepaths

--- examples/GW190521/etc/GW190521_generate_waveforms.sh
+++ examples/GW190521/etc/GW190521_generate_waveforms.sh
@@ -2,7 +2,7 @@
 
 wfs=$(which waveform_h5s)
 
-# Optional: check if it's found
+# Check if it's found
 if [[ -z "$wfs" ]]; then
     echo "Error: waveform_h5s not found in PATH."
     exit 1
@@ -11,5 +11,5 @@ fi
 uv run $wfs --directory output --run_key full --N_waveforms 1000 --ncpu 1
 
 for t in -0.05 0 0.05: 
-    uv run $wfs --directory output --run_key pre_${t}seconds --N_waveforms 1000 --ncpu 1
-    uv run $wfs --directory output --run_key post_${t}seconds --N_waveforms 1000 --ncpu 1
+    uv run $wfs --directory ../output --run_key pre_${t}seconds --N_waveforms 1000 --ncpu 1
+    uv run $wfs --directory ../output --run_key post_${t}seconds --N_waveforms 1000 --ncpu 1
