8ded40ecda2c74311cf4da21e0cdaeba808cfbda
Simona Miller <simona.miller@ldas-pcdev3.ligo.caltech.edu>
2025-08-22 12:27:35 -0700

    update postprocessing for condor; verify results in GW190521 example

--- examples/GW190521/etc/GW190521_generate_waveforms.sh
+++ examples/GW190521/etc/GW190521_generate_waveforms.sh
@@ -8,8 +8,14 @@ if [[ -z "$wfs" ]]; then
     exit 1
 fi
 
-uv run $wfs --directory output --run_key full --N_waveforms 1000 --ncpu 1
+# Generate waveforms for full run
+cd ../output
+$wfs --directory . --run_key full --N_waveforms 1000 --ncpu 1
 
-for t in -0.05 0 0.05: 
-    uv run $wfs --directory ../output --run_key pre_${t}seconds --N_waveforms 1000 --ncpu 1
-    uv run $wfs --directory ../output --run_key post_${t}seconds --N_waveforms 1000 --ncpu 1
+# Generate waveforms for pre/post cutoff runs 
+for t in -0.05 0.0 0.05; do
+    $wfs --directory . --run_key pre_${t} --N_waveforms 1000 --ncpu 1
+    $wfs --directory . --run_key post_${t} --N_waveforms 1000 --ncpu 1
+done
+
+cd - 
